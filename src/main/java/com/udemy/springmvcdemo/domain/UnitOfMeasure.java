62d075376b0241b065459858c4dbfc313051e8f5
js <jsbsmp@gmail.com>
2018-02-28 10:59:52 +0200

    JPA Data Modeling Assignment:
    - create UnitOfMeasure Entity
    - create a unidirectional relationship from Ingredient to UnitOfMeasure
    - do not cascade persistence events from Ingredient to UnitOfMeasure

---
+++ src/main/java/com/udemy/springmvcdemo/domain/UnitOfMeasure.java
@@ -0,0 +1,28 @@
+package com.udemy.springmvcdemo.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
