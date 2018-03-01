9af005d55ca519decec7e0bfd4c30e7e00ca55bd
js <jsbsmp@gmail.com>
2018-03-01 11:20:28 +0200

    Testing Spring Framework Applications: Using Mockito Mocks

---
+++ src/test/java/com/udemy/springmvcdemo/services/RecipeServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.udemy.springmvcdemo.services;
+
+import com.udemy.springmvcdemo.domain.Recipe;
+import com.udemy.springmvcdemo.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RecipeServiceImplTest {
+
+    private RecipeServiceImpl recipeService;
+
+    @Mock
+    private RecipeRepository recipeRepository;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getAllRecipes() throws Exception {
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipesData = new HashSet<>();
+        recipesData.add(recipe);
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+        Set<Recipe> recipes = recipeService.getAllRecipes();
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
