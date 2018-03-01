5730e5e45ffffc5a7059d97afddd1a18e6a19172
js <jsbsmp@gmail.com>
2018-03-01 09:30:04 +0200

    Project Lombok

--- src/main/java/com/udemy/springmvcdemo/services/RecipeServiceImpl.java
+++ src/main/java/com/udemy/springmvcdemo/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.udemy.springmvcdemo.services;
 
 import com.udemy.springmvcdemo.domain.Recipe;
 import com.udemy.springmvcdemo.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getAllRecipes() {
+        log.debug("in the service");
         Set<Recipe> recipes = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(e -> recipes.add(e));
         return recipes;
